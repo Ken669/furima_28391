f3d01bc4a51d1165b749dcba90746f679dace6de
Ken669 <hemetuzo@honeys.be>
2020-08-04 17:58:37 +0900

    prepare FactoryBot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname {Faker::Internet.username}
+    email {Faker::Internet.email}
+    password {Faker::Internet.password(
+      min_length: 6, max_length: 20, mix_case: true
+    )}
+    password_confirmation {password}
+    first_name {Gimei.last.kanji}
+    last_name {Gimei.first.kanji}
+    first_name_kana {Gimei.last.katakana}
+    last_name_kana {Gimei.first.katakana}
+    birthday {Faker::Date.in_date_period(year: 2000)}
   end
 end
