d0c59f846ae8641699c13f98aab9d92988d7fe71
Ken669 <hemetuzo@honeys.be>
2020-08-08 19:18:38 +0900

    model test done

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'registration test' do
+    before do
+      @address = FactoryBot.build(:address)
+    end
+
+    context 'done successfully on the following cases' do
+      it 'when all are filled' do
+        expect(@address.valid?).to eq true
+      end
+      
+      it 'without building name' do
+        @address.building_name = nil
+        expect(@address.valid?).to eq true
+      end
+    end
+
+    context 'returns fail on the following cases' do
+      it 'postal_code is nil' do
+        @address.postal_code = nil
+        expect(@address.valid?).to eq false
+      end
+      it 'postal_code do not have hyphen' do
+        @address.postal_code = Faker::Number.number(digits: 7)
+        expect(@address.valid?).to eq false
+      end
+
+      it 'prefecture_id is nil' do
+        @address.prefecture_id = 0
+        expect(@address.valid?).to eq false
+      end
+
+      it 'city is nil' do
+        @address.city = nil
+        expect(@address.valid?).to eq false
+      end
+
+      it 'town is nil' do
+        @address.town = nil
+        expect(@address.valid?).to eq false
+      end
+
+      it 'phone is nil' do
+        @address.phone = nil
+        expect(@address.valid?).to eq false
+      end
+      it 'phone is less than 10 digits' do
+        @address.phone = Faker::Number.decimal_part(digits: 9)
+        expect(@address.valid?).to eq false
+      end
+    end
+  end
 end
