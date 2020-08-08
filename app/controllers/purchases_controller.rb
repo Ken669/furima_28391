57584729cfb02aea5739757bd14a01ab3a198d7c
Ken669 <hemetuzo@honeys.be>
2020-08-08 11:37:23 +0900

    purchase form done

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -9,10 +9,14 @@ class PurchasesController < ApplicationController
     @address = Address.new(address_params)
     purchase = Purchase.new(item_id: params[:item_id], user_id: current_user.id)
     @item = purchase.item
-    # binding.pry
     if @address.valid? && purchase.valid?
+      binding.pry
+      buy_item
+      @address.save
+      purchase.save
       @item.sold_out = true
-      # binding.pry
+      binding.pry
+      # redirect_to item_purchases_path(@item)
     else
       render :new
     end
@@ -23,7 +27,19 @@ class PurchasesController < ApplicationController
   def address_params
     params.require(:address).permit(
       :postal_code, :prefecture_id, :city,
-      :town, :building_name, :phone
+      :town, :building_name, :phone,
+      :token
     ).merge(user_id: current_user.id)
+  end
+
+  def buy_item
+    Payjp.api_key = 'sk_test_9e00c98c40b3f79f09364de1'
+    # Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: address_params[:token],
+      currency: 'jpy'
+    )
+    binding.pry
   end
 end
